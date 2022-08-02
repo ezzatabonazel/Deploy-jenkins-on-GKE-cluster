9d4f8eab5190fef78a1d8d4deec0082974a80315
ezzatabonazel <ezzat.kadry.abonazel@gmail.com>
2022-08-02 05:15:53 +0200

    use nat for all vpc

--- terraform/network/natgateway.tf
+++ terraform/network/natgateway.tf
@@ -4,10 +4,6 @@ resource "google_compute_router_nat" "mynat_gateway" {
   region = var.managment_subnet_region
 
   nat_ip_allocate_option = "AUTO_ONLY"
-
-  source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
-  subnetwork {
-    name                    = google_compute_subnetwork.management_subnet.id
-    source_ip_ranges_to_nat = ["PRIMARY_IP_RANGE"]
-  }
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+  
 }
